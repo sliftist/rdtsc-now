a8c53b6e10ff7d3e0fb67e3e06c6480474f44e4a
sliftist <sliftist@gmail.com>
2025-02-28 12:17:07 -0500

    Make building optional.

--- index.js
+++ index.js
@@ -1,81 +1,89 @@
 module.allowclient = true;
 
+let canCompile = false;
+let isNode = typeof document === "undefined";
+
+if (isNode) {
+    try {
+        require("./build/Release/binding.node");
+        canCompile = true;
+    } catch {}
+}
+
 // If we are in the browser, just use performance.now
-if (typeof document !== "undefined") {
+if (!isNode || !canCompile) {
     module.exports.now = function now() {
         return performance.now();
     };
-    return;
-}
+} else {
+    const binding = require("./build/Release/binding.node");
+    const rdtsc = binding.rdtsc;
 
-const binding = require("./build/Release/binding.node");
-const rdtsc = binding.rdtsc;
+    // Massive overkill at estimating the cycles per millisecond, but... at least the code
+    //  should work in other environments?
+    const precisionCycles = 10;
+    const clocksPerMilliEstimate = 100 * 1000;
+    // Our precision depends on the baseTime precision, so this is how many times precision
+    //  we want of that.
+    const targetPrecision = 100;
+    // Limit the test time, as baseTime might have a very low precision (10ms was possible
+    //  in some browsers, as some times, due to security reasons), and we don't want to block
+    //  startup for multiple seconds!
+    const maxTestTime = 50;
+    // Test a few times, in case there is a context switch during a test
+    const testCount = 3;
 
-// Massive overkill at estimating the cycles per millisecond, but... at least the code
-//  should work in other environments?
-const precisionCycles = 10;
-const clocksPerMilliEstimate = 100 * 1000;
-// Our precision depends on the baseTime precision, so this is how many times precision
-//  we want of that.
-const targetPrecision = 100;
-// Limit the test time, as baseTime might have a very low precision (10ms was possible
-//  in some browsers, as some times, due to security reasons), and we don't want to block
-//  startup for multiple seconds!
-const maxTestTime = 50;
-// Test a few times, in case there is a context switch during a test
-const testCount = 3;
+    //let time = Date.now();
+    const milliPerClock = getMilliPerClock();
+    //console.log(`Took ${Date.now() - time}ms to estimate cycles per millisecond.`);
 
-//let time = Date.now();
-const milliPerClock = getMilliPerClock();
-//console.log(`Took ${Date.now() - time}ms to estimate cycles per millisecond.`);
-
-
-function baseTime() {
-    return performance.now();
-}
-function getMilliPerClock() {
-    // Will get borked if there is a context switch, but... should still be pretty good at detecting
-    //  reduced precision of baseTime (which is used as a security feature in browsers).
-    let timerPrecision = 0.001;
-    for(let i = 0; i < precisionCycles; i++) {
-        let startTime = baseTime();
-        let endTime = baseTime();
-        while (startTime === endTime) {
-            endTime = baseTime();
+    function baseTime() {
+        return performance.now();
+    }
+    function getMilliPerClock() {
+        // Will get borked if there is a context switch, but... should still be pretty good at detecting
+        //  reduced precision of baseTime (which is used as a security feature in browsers).
+        let timerPrecision = 0.001;
+        for (let i = 0; i < precisionCycles; i++) {
+            let startTime = baseTime();
+            let endTime = baseTime();
+            while (startTime === endTime) {
+                endTime = baseTime();
+            }
+            let precision = endTime - startTime;
+            if (precision > timerPrecision) {
+                timerPrecision = precision;
+            }
         }
-        let precision = endTime - startTime;
-        if (precision > timerPrecision) {
-            timerPrecision = precision;
+
+        let targetTime = targetPrecision * timerPrecision;
+        if (targetTime > maxTestTime) {
+            targetTime = maxTestTime;
         }
+        const clockCycles = clocksPerMilliEstimate * targetTime;
+        //console.log({ clockCycles, targetTime, timerPrecision });
+        let fastest = Number.MAX_SAFE_INTEGER;
+        for (let i = 0; i < testCount; i++) {
+            let time = Date.now();
+            let clock = rdtsc();
+            for (let i = 0; i < clockCycles; i++) {
+                rdtsc();
+            }
+            clock = rdtsc() - clock;
+            time = Date.now() - time;
+            let timePer = time / clock;
+            if (timePer < fastest) {
+                fastest = timePer;
+            }
+        }
+        return fastest;
     }
 
-    let targetTime = targetPrecision * timerPrecision;
-    if (targetTime > maxTestTime) {
-        targetTime = maxTestTime;
+    function now() {
+        return rdtsc() * milliPerClock;
     }
-    const clockCycles = clocksPerMilliEstimate * targetTime;
-    //console.log({ clockCycles, targetTime, timerPrecision });
-    let fastest = Number.MAX_SAFE_INTEGER;
-    for(let i = 0; i < testCount; i++) {
-        let time = Date.now();
-        let clock = rdtsc();
-        for (let i = 0; i < clockCycles; i++) {
-            rdtsc();
-        }
-        clock = rdtsc() - clock;
-        time = Date.now() - time;
-        let timePer = time / clock;
-        if (timePer < fastest) {
-            fastest = timePer;
-        }
-    }
-    return fastest;
+    module.exports.now = now;
 }
-
-function now() {
-    return rdtsc() * milliPerClock;
-}
-module.exports.now = now;
 
 // main();
 // async function main() {
